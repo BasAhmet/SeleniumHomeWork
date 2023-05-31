049032afd30fcfa32cf699bedfd7b2c632f1b843
Ahmet Baş <ahba8184@gmail.com>
2023-05-31 13:04:02 +0300

    Selenium 7. homework

--- src/test/java/homework/Util.java
+++ src/test/java/homework/Util.java
@@ -1,11 +1,21 @@
 package homework;
 
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
 public class Util {
     public static String setChromeProperty(){
         String chromeDriver = "src/resources/drivers/chromedriver.exe";
         return System.setProperty("chromeDriver", chromeDriver);
     }
-    public static void sleep(int a) throws InterruptedException {
-        Thread.sleep(a*1000);
+    public static void sleep(int time) throws InterruptedException {
+        Thread.sleep(time*1000);
+    }
+    public static void maximize(WebDriver driver){
+        driver.manage().window().maximize();
+    }
+    public static void duration(WebDriver driver,int time){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
     }
 }
