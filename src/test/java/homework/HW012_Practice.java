72e3071377f69b68c31808ab83617a92a8c6b6e6
Ahmet Baş <ahba8184@gmail.com>
2023-06-10 09:12:36 +0300

    homework 12 done

---
+++ src/test/java/homework/HW012_Practice.java
@@ -0,0 +1,73 @@
+package homework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.Assert.*;
+
+public class HW012_Practice {
+    /*
+    Verilen web sayfasına gidin.
+    https://the-internet.herokuapp.com/checkboxes
+    Checkbox1 ve checkbox2 elementlerini locate edin.
+    Checkbox1 seçili değilse onay kutusunu tıklayın
+    Checkbox2 seçili değilse onay kutusunu tıklayın
+     */
+    WebDriver driver;
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        // Checkbox1 ve checkbox2 elementlerini locate edin.
+        WebElement checkBox1 = driver.findElement(By.xpath("(//input)[1]"));
+        WebElement checkBox2 = driver.findElement(By.xpath("(//input)[2]"));
+
+        // Checkbox1 seçili değilse onay kutusunu tıklayın
+        Thread.sleep(3000);
+        if(!checkBox1.isSelected()){
+            checkBox1.click();
+        }
+
+        // Checkbox2 seçili değilse onay kutusunu tıklayın
+        if(checkBox2.isSelected()){
+            checkBox2.click();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
