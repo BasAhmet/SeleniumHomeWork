8b9408b77be5aa4a4fd8d993e5fbe7f615b67ea5
Ahmet Baş <ahba8184@gmail.com>
2023-06-17 23:39:15 +0300

    New Homework Add

--- src/test/java/homework/HW_AutomationExercise_TestCase.java
+++ src/test/java/homework/HW_AutomationExercise_TestCase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class HW_AutomationExercise_TestCase {
     public static void main(String[] args) {
         Util.setChromeProperty();
@@ -16,11 +18,18 @@ public class HW_AutomationExercise_TestCase {
         driver.get("https://automationexercise.com/test_cases");
         //test caseleri alınız
         int i = 1;
+
         WebElement testCaseHeader;
         while (true){
             testCaseHeader = driver.findElement(By.xpath("(//*[@class='panel-title'])["+ i + "]"));
             if(!(testCaseHeader.getText().equals("Feedback for Us"))){
                 System.out.println(testCaseHeader.getText());
+                testCaseHeader.click();
+                Util.sleep(2);
+                List<WebElement> liste = driver.findElements(By.xpath("//*[@id='collapse" + i + "']/ul/li"));
+                for (int j = 1 ; j<liste.size()+1 ; j++) {
+                    System.out.println(driver.findElement(By.xpath("//*[@id='collapse" + i + "']/ul/li[" + j + "]")).getText());
+                }
                 i++;
             }else{
                 break;
@@ -32,7 +41,7 @@ public class HW_AutomationExercise_TestCase {
 
         //*[@id="collapse1"]
 
-        driver.close();
+        //driver.close();
 
 
     }
