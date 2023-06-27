6b670034932611dd5441954b74812e8194534d35
Ahmet Baş <ahba8184@gmail.com>
2023-06-27 08:27:24 +0300

    Homework

--- src/test/java/homework/HW027_Lms_Zoom_Giris.java
+++ src/test/java/homework/HW027_Lms_Zoom_Giris.java
@@ -1,6 +1,8 @@
 package homework;
 
+import com.github.javafaker.Faker;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
@@ -21,10 +23,19 @@ public class HW027_Lms_Zoom_Giris extends TestBase {
 
         //outomation engineer linkine tıklayınız
         Actions actions = new Actions(driver);
-        actions.moveToElement(xpath("(//*[@class='list-inline-item'])[5]/a")).perform();
+        actions.moveToElement(xpath("(//*[@class='list-inline-item'])[5]")).perform();
         actions.click().perform();
 
         //go to zoom tıklayınız
         xpath("//*[@class='banner-btn']").click();
+
+        //açılan yeni pencereye geçiniz
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        pause(2);
+
+        //çıkan alert e tıklayınız
+        actions = new Actions(driver);
+        actions.sendKeys(Keys.SPACE).perform();
     }
+
 }
